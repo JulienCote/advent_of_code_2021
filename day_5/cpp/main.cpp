2f976cd0b8a5e4fb6317e7149f9c02ee2ba899eb
Julien C <julien.jc.cote@gmail.com>
2021-12-06 20:25:32 -0500

    Day 5 - Fix parsing bug and a couple logic errors in the logic

--- day_5/cpp/main.cpp
+++ day_5/cpp/main.cpp
@@ -14,32 +14,35 @@ int main()
   std::vector<std::array<int,4>> coordinates;
   for (const auto& str : file)
   {
-    auto it_begin = std::sregex_iterator(str.begin(), str.end(), parse_coordinates);
+    auto matches = std::sregex_iterator(str.cbegin(), str.cend(), parse_coordinates);
+    if (matches->size() != 5) //elemnt 0 is the whole string + 4 actual matches
+      throw std::runtime_error("Coordinates must have 4 values");
 
-    //TODO: this doesn't work, only the first value is valid
     coordinates.emplace_back(
       std::array<int,4>{
-        std::atoi(it_begin->str().c_str()),
-        std::atoi((++it_begin)->str().c_str()),
-        std::atoi((++it_begin)->str().c_str()),
-        std::atoi((++it_begin)->str().c_str())});
+        std::stoi((*matches)[1]),
+        std::stoi((*matches)[2]),
+        std::stoi((*matches)[3]),
+        std::stoi((*matches)[4])});
   }
 
   std::map<std::pair<int,int>, size_t> count_straight;
   std::map<std::pair<int,int>, size_t> count_all;
 
+  // keep track of how many times each coordinate is visited
   for (auto& c : coordinates)
   {
     const bool same_x = c[0] == c[2];
     const bool same_y = c[1] == c[3];
-    const bool is_diagonal = same_x || same_y;
-    const int delta_x = (same_x ? 0 : (c[0] > c[2] ? 1 : -1));
-    const int delta_y = (same_y ? 0 : (c[1] > c[3] ? 1 : -1));
+    const bool is_diagonal = !(same_x || same_y);
+    const int delta_x = (same_x ? 0 : (c[0] > c[2] ? -1 : 1));
+    const int delta_y = (same_y ? 0 : (c[1] > c[3] ? -1 : 1));
 
-
-    while (c[0] != c[2] || c[1] != c[3])
+    bool once_more = true;
+    while (once_more) //go through every points in between coordinates
     {
-      std::cout << c[0] << ", " << c[1] << ", " << c[2] << ", " << c[3] << std::endl;
+      if (c[0] == c[2] && c[1] == c[3])
+        once_more = false;
 
       auto key = std::make_pair(c[0], c[1]);
       if (!is_diagonal)
@@ -64,7 +67,6 @@ int main()
 
   size_t answer_one = count_dangerous_vents(count_straight);
   size_t answer_two = count_dangerous_vents(count_all);
-
 
   std::cout << "First Answer: " << answer_one << std::endl;
   std::cout << "Second Answer: " << answer_two << std::endl;
