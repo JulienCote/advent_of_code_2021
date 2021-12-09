4fbd037e82e9af92309dc50bbd99eec990a72511
Julien C <julien.jc.cote@gmail.com>
2021-12-08 22:28:16 -0500

    First star of Day 8 in Q

---
+++ day_8/cpp/main.cpp
@@ -0,0 +1,67 @@
+#include "../../common/cpp/load_data.h"
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <numeric>
+#include <regex>
+
+#include <unordered_map>
+
+namespace
+{
+  int get_triangular_sum(int n)
+  {
+    static std::vector<int> cache {0, 1};
+    if (cache.size() > n)
+      return cache[n];
+    int new_sum = n + get_triangular_sum(n - 1);
+    cache.emplace_back(new_sum);
+    return new_sum;
+  }
+
+  // assumes sorted crabs
+  int first_star(const std::vector<int>& crabs)
+  {
+    const int median_crab = crabs.at(crabs.size() / 2);
+
+    int total_distance_from_median = 0;
+    for (auto c : crabs)
+      total_distance_from_median += std::abs(c - median_crab);
+
+    return total_distance_from_median;
+  }
+
+  // assumes sorted crabs
+  int second_star(const std::vector<int>& crabs)
+  {
+    const int min_crab = crabs.front();
+    const int max_crab = crabs.back();
+    const int crabRange = max_crab - min_crab;
+
+    std::vector<int> gas_sums_by_position;
+    gas_sums_by_position.resize(crabRange);
+
+    for (auto c : crabs)
+    {
+      for (int i = c; i >= min_crab; --i)
+        gas_sums_by_position[i] += get_triangular_sum(std::abs(c - i));
+
+      for (int i = c; i <= max_crab; ++i)
+        gas_sums_by_position[i] += get_triangular_sum(std::abs(c - i));
+    }
+
+   return *std::min_element(gas_sums_by_position.cbegin(), gas_sums_by_position.cend());
+  }
+}
+
+int main()
+{
+  auto file = load_data::load_file("../data", ',', 0, std::numeric_limits<size_t>::max());
+  std::vector<int> crabs = load_data::parse_string<int>(file[0]);
+  std::sort(crabs.begin(), crabs.end()); // everyone needs sorted data
+
+  std::cout << "First Answer: " << first_star(crabs) << std::endl;
+  std::cout << "Second Answer: " << second_star(crabs) << std::endl;
+
+}
--- day_8/q/main.q
+++ day_8/q/main.q
@@ -1,17 +1,19 @@
 /this line sets the pwd in the directory of the this file
 system "cd ", first[system "pwd"],"/", 1_string first ` vs  hsym .z.f
 
-/read data as a list of pairs (input list;output list)
-data:" " vs'' (" | " vs) each read0 `:../data
+/read data as a list of longs
+data:"J"$ "," vs raze read0 `:../data
 
 first_star:{[data]
-  /count how many output strings contain 2, 3, 4 or 7 characters
-  :sum over {x in 2 4 3 7} each {count each x} each last each data
+  final_position:`long$med data;
+  :sum abs data - final_position
   }
 
 / TODO: implement this
 second_star:{[data]
-  segment_per_number:0 1 2 3 4 5 6 7 8 9!6 2 5 5 4 5 6 3 7 6;
+  min_data:sums each 1 + til each abs data - min data;
+  max_data:sums each 1 + til each abs data - max data;
+  :min sum reverse'[min_data],' 0,' max_data
   }
 
 -1 "First start value is: ", string first_star[data];
